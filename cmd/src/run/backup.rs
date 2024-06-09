e324cd2e8dd56b6ee8086cb11ab06390271bf937
roylaurie <roy.laurie.pub@asmov.software>
2024-06-08 18:05:50 -0700

    logo

--- cmd/src/run/backup.rs
+++ cmd/src/run/backup.rs
@@ -1,10 +1,18 @@
 use std::process;
 use crate::{paths, config, cli, Result};
 
-pub fn run_backup(cli: &cli::Cli, _subcmd: &cli::BackupCommand) -> Result<process::ExitCode> {
+pub fn run_backup(cli: &cli::Cli, subcmd: &cli::BackupCommand) -> Result<process::ExitCode> {
     let config = config::read_config(cli.config_file.as_ref())?;
     verify_environment(&config)?;
-    Ok(std::process::ExitCode::SUCCESS)
+
+    match subcmd {
+        cli::BackupCommand::Scheduled => run_backup_scheduled(cli),
+    }
+}
+
+pub fn run_backup_scheduled(cli: &cli::Cli) -> Result<process::ExitCode> {
+    dbg!(cli);
+    todo!()
 }
 
 /// Verify that the runtime environment that has been configured is valid.  
